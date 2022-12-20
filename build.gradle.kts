ec33ed4302c4e820964efa45cf1fda3442f83c0c
Jasper de Vries <jasper.devries@atradius.com>
2022-12-20 11:25:48 +0100

    Axion release plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,10 @@
 plugins {
     id("com.diffplug.spotless") version ("6.11.0")
     id("org.owasp.dependencycheck") version ("7.4.1") apply(false)
+    id("pl.allegro.tech.build.axion-release") version("1.14.3")
 }
+
+project.version = scmVersion.version
 
 allprojects {
     repositories {
